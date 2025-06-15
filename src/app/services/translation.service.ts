4d2c26cac9474d9630ed44195f1246f820e47247
Antoine Dausque <antoine.dausque@hotmail.fr>
2025-06-15 18:42:29 +0200

    Add translation architecture (JSON, Services, Pipes)

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TranslationService {
+  private language = new BehaviorSubject<string>('fr');
+  private translations: any = {};
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.loadTranslations('fr');
+  }
+
+  setLanguage(lang: string) {
+    this.language.next(lang);
+    this.loadTranslations(lang);
+  }
+
+  getCurrentLanguage() {
+    return this.language.value;
+  }
+
+  loadTranslations(lang: string) {
+    this.http.get(`assets/i18n/${lang}.json`).subscribe((data: any) => {
+      this.translations = data;
+    });
+  }
+
+  translate(key: string): string {
+    return key.split('.').reduce((obj, k) =>
+      (obj && obj[k] !== undefined ? obj[k] : null), this.translations) || key;
+  }
 }
